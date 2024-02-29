47c79db5a5680d0dd8a31eeb3e54f717a3e1646c
MiriSafra <m0533175827@gmail.com>
2024-02-29 20:17:20 +0200

    change

--- HtmlSerializer/Program.cs
+++ HtmlSerializer/Program.cs
@@ -7,8 +7,8 @@ Serializer htmlSerializer = new Serializer();
 var html =await htmlSerializer.Load("https://forum.netfree.link/category/1/%D7%94%D7%9B%D7%A8%D7%96%D7%95%D7%AA?");
 HtmlElement htmlElement= htmlSerializer.BuildHtmlTree(html);
 Selector selector = Selector.Parse("ul.nav.navbar-nav");
-var result=HtmlElementExtensions.FindElementsBySelector(htmlElement, selector);
-foreach (var item in result)
+var results = HtmlElementExtensions.FindElementsBySelector(htmlElement, selector);
+foreach (var item in results)
 {
     Console.Write(item.Name + " " + item.Id + " ");
 	foreach (var c in item.Classes)
